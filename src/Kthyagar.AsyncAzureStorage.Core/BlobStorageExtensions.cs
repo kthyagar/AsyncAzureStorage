3ca476969148bf5a792f7a98c0c283a778b0a5ce
Karthi <kthyagar@hotmail.com>
2013-03-06 20:14:20 -0600

    Initial Creation
    The initial push only has support for blob storage methods

---
+++ src/Kthyagar.AsyncAzureStorage.Core/BlobStorageExtensions.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Blob;
+
+namespace Kthyagar.AsyncAzureStorage
+{
+    public static class BlobStorageExtensions
+    {
+        /// <summary>
+        /// CreateIfNotExistsAsync
+        /// </summary>
+        /// <param name="blobContainer"></param>
+        /// <returns></returns>
+        public static Task CreateIfNotExistsAsync(this CloudBlobContainer blobContainer)
+        {
+            var ar = blobContainer.BeginCreateIfNotExists(null, null);
+
+            return Task.Factory.FromAsync(ar, (r) => { blobContainer.EndCreateIfNotExists(r); });
+        }
+
+        /// <summary>
+        /// GetBlobReferenceFromServerAsync
+        /// </summary>
+        /// <param name="blobContainer"></param>
+        /// <param name="blobName"></param>
+        /// <returns></returns>
+        public static Task<ICloudBlob> GetBlobReferenceFromServerAsync(this CloudBlobContainer blobContainer, string blobName)
+        {
+            var retVal = blobContainer.BeginGetBlobReferenceFromServer(blobName, null, null);
+            IAsyncResult arRetVal = retVal as IAsyncResult;
+
+
+            return Task.Factory.FromAsync(arRetVal,
+                (Func<IAsyncResult, ICloudBlob>)blobContainer.EndGetBlobReferenceFromServer);
+        }
+
+        /// <summary>
+        /// DownloadToStreamAsync
+        /// </summary>
+        /// <param name="blob"></param>
+        /// <param name="stream"></param>
+        /// <returns></returns>
+        public static async Task DownloadToStreamAsync(this ICloudBlob blob, Stream stream)
+        {
+            var retVal = blob.BeginDownloadToStream(stream, null, null);
+            IAsyncResult arRetVal = retVal as IAsyncResult;
+
+            await Task.Factory.FromAsync(arRetVal, (r) => { blob.EndDownloadToStream(r); });
+
+            stream.Seek(0, 0);
+        }
+
+        /// <summary>
+        /// DownloadTextAsync
+        /// </summary>
+        /// <param name="blob"></param>
+        /// <returns></returns>
+        public static async Task<string> DownloadTextAsync(this ICloudBlob blob)
+        {
+            using (var memStream = new MemoryStream())
+            {
+                await blob.DownloadToStreamAsync(memStream);
+                memStream.Position = 0;
+                using (var strRdr = new StreamReader(memStream))
+                {
+                    var str = strRdr.ReadToEnd();
+                    return str;
+                } // using
+            } // using
+        }
+
+        /// <summary>
+        /// UploadFromStreamAsync
+        /// </summary>
+        /// <param name="blob"></param>
+        /// <param name="sourceStream"></param>
+        /// <param name="accessCondition"></param>
+        /// <param name="options"></param>
+        /// <param name="operationContext"></param>
+        /// <returns></returns>
+        public static async Task UploadFromStreamAsync(this ICloudBlob blob, Stream sourceStream, AccessCondition accessCondition, BlobRequestOptions options, OperationContext operationContext)
+        {
+            Func<AsyncCallback, Object, IAsyncResult> beginMethod = (cb, state) =>
+            {
+                return blob.BeginUploadFromStream(sourceStream, accessCondition, options, operationContext, cb, state);
+            };
+
+            Action<IAsyncResult> endMethod = (r) =>
+            {
+                blob.EndUploadFromStream(r);
+            };
+
+            await Task.Factory.FromAsync(beginMethod, endMethod, null);
+        }
+
+        /// <summary>
+        /// UploadTextAsync
+        /// </summary>
+        /// <param name="blob"></param>
+        /// <param name="text"></param>
+        /// <returns></returns>
+        public static async Task UploadTextAsync(this ICloudBlob blob, string text)
+        {
+            using (var memStream = new MemoryStream())
+            {
+                using (var strWriter = new StreamWriter(memStream))
+                {
+                    strWriter.Write(text);
+                    strWriter.Flush();
+                    memStream.Position = 0;
+
+                    await blob.UploadFromStreamAsync(memStream, null, null, null);
+                } // using
+            } // using
+        }
+    } // class
+} // namespace
---
+++ src/Kthyagar.AsyncAzureStorage.Core/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Kthyagar.AsyncAzureStorage.Core")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Kthyagar.AsyncAzureStorage.Core")]
+[assembly: AssemblyCopyright("Copyright ©  2013")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("42f7473a-8d12-4292-96a0-81744b745ea1")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
